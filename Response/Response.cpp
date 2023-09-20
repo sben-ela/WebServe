55970cfe14a3a30a52767a0ed1af9576f4595211
Salaheddine Ben-elarradia <sben-ela@c4r6p9.1337.ma>
2023-09-20 21:53:16 +0100

    modifResponse

--- Response/Response.cpp
+++ Response/Response.cpp
@@ -6,31 +6,29 @@
 /*   By: sben-ela <sben-ela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 11:36:51 by sben-ela          #+#    #+#             */
-/*   Updated: 2023/09/17 14:39:49 by sben-ela         ###   ########.fr       */
+/*   Updated: 2023/09/20 21:52:26 by sben-ela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Response.hpp"
 
 #define FILESIZE 10
+#define EXFIALE 13 
 
 
 void    SeparatesHeader(std::string& str)
 {
     str = str.substr(str.find("\r\n\r\n") + 4);
-    std::cout << str << std::endl;
 }
 
 std::string	CgiExecute(const Client &client)
 {
 	int fd[2];
-    ssize_t bytesRead;
     std::string output;
-    char buffer[1024];
 
     std::map<std::string, char*> intrepreter;
     intrepreter[".php"] = (char *)"/usr/bin/php-cgi";
-    intrepreter[".py"] = (char*)"/usr/bin/python";
+    intrepreter[".py"] = (char*)"/usr/bin/python3";
 	char *Path[3] = {intrepreter[client.GetFileExtention()], (char *)client.GetPath().c_str(), NULL};
 
 	pipe(fd);
@@ -40,16 +38,16 @@ std::string	CgiExecute(const Client &client)
 		dup2(fd[1], 1);
 		execve(Path[0], Path, 0);
 		std::cout << "ERRRRORRR" << std::endl;
-        exit(0);
+        exit(EXFIALE);
 	}
 	waitpid(pid, 0, 0);
     close(fd[1]);
+    ssize_t bytesRead;
+    char buffer[1024];
     while ((bytesRead = read(fd[0], buffer, sizeof(buffer))) > 0)
         output.append(buffer, bytesRead);
     if (client.GetFileExtention() == ".php")
         SeparatesHeader(output);
-	else
-		(void)fd;
 	return(output);
 }
 
@@ -73,22 +71,39 @@ void    Get(const Client &client)
     char buff[BUFFER_SIZE];
     std::string header;
     int fd;
+	int pfd[2];
+
 
     header = client.GetHttpVersion() + " 200 OK\r\nContent-Type: "
-    + client.GetConetType() + "\r\ncontent-length: 11" + "\r\n\r\n";
+    + client.GetConetType() + "\r\ncontent-length: 11" + "\r\n\r\n"; // ! content lenght with stat function
 
     send(client.GetSocketId(), header.c_str(), header.size(), 0);
     if (client.GetFileExtention() == ".php" || client.GetFileExtention() == ".py")
     {
-        std::string out = CgiExecute(client);
-        std::cout << out << std::endl;
-        int rd = write(client.GetSocketId(), out.c_str(), out.size());
-		(void)rd;
-        return ;
+        std::map<std::string, char*> intrepreter;
+        intrepreter[".php"] = (char *)"/usr/bin/php-cgi";
+        intrepreter[".py"] = (char*)"/usr/bin/python3";
+        char *Path[3] = {intrepreter[client.GetFileExtention()], (char *)client.GetPath().c_str(), NULL};
+
+        pipe(pfd);
+        int pid  = fork();
+        if (!pid)
+        {
+            dup2(pfd[1], 1);
+            execve(Path[0], Path, 0);
+            std::cout << "ERRRRORRR" << std::endl;
+            exit(EXFIALE);
+        }
+        waitpid(pid, 0, 0);
+        close(pfd[1]);
+        fd = pfd[0];
     }
-    fd = open (client.GetPath().c_str(), O_RDONLY);
-    if (fd < 0)
-        throw(std::runtime_error("open Path in Get failed"));
+    else
+    {
+        fd = open (client.GetPath().c_str(), O_RDONLY);
+        if (fd < 0)
+            throw(std::runtime_error("open Path in Get failed"));
+    }
     while (read(fd, buff, BUFFER_SIZE) > 0)
         if (write (client.GetSocketId() , buff, BUFFER_SIZE) < 0)
             throw(std::runtime_error("Write buff in Get Failed"));
@@ -124,52 +139,52 @@ void    ft_Response(const Client &client)
     //     Delete();
 }
 
-// #include <iostream>
-// #include <sys/socket.h>
-// #include <netinet/in.h>
-// #include <cstring>
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstring>
 
 
-// int main() {
+int main() {
 
-//     int server_fd, new_socket;
-//     struct sockaddr_in address;
-//     int addrlen = sizeof(address);
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
 
-//     // Create socket file descriptor
-//     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-//         perror("Socket creation failed");
-//         return 1;
-//     }
+    // Create socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket creation failed");
+        return 1;
+    }
 
-//     address.sin_family = AF_INET;
-//     address.sin_addr.s_addr = INADDR_ANY;
-//     address.sin_port = htons(PORT);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
 
-//     // Bind the socket to the specified address and port
-//     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-//         perror("Bind failed");
-//         return 1;
-//     }
+    // Bind the socket to the specified address and port
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        return 1;
+    }
 
-//     // Listen for incoming connections
-//     if (listen(server_fd, 3) < 0) {
-//         perror("Listen failed");
-//         return 1;
-//     }
+    // Listen for incoming connections
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen failed");
+        return 1;
+    }
 
-//     // std::cout << "Listening on port " << PORT << "..." << std::endl;
+    // std::cout << "Listening on port " << PORT << "..." << std::endl;
 
-//     // Accept a new connection
-//     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
-//         perror("Accept failed");
-//         return 1;
-//     }
-//     try
-//     {
-//         ft_Response(Client("GET" , "test.py", ".py",  "text/plain", "HTTP/1.1 ", new_socket));
-//     }
-//     catch(std::exception &e){
-//         std::cout << "Error : " << e.what() << std::endl;
-//     }
-// }
+    // Accept a new connection
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+        perror("Accept failed");
+        return 1;
+    }
+    try
+    {
+        ft_Response(Client("GET" , "test.py", ".py",  "text/plain", "HTTP/1.1 ", new_socket));
+    }
+    catch(std::exception &e){
+        std::cout << "Error : " << e.what() << std::endl;
+    }
+}
